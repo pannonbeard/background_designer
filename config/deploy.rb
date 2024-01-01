88a08c09edf5e0cd5c94852d6e04e8ab11aeb701
Phil Hendricks <pfhendri1@gmail.com>
2023-12-31 18:51:19 -0600

    added link passenger file

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,7 +22,7 @@ set :deploy_to, "/home/deploy/apps/#{fetch :application}"
 # set :pty, true
 
 # Default value for :linked_files is []
-# append :linked_files, "config/database.yml", 'config/master.key'
+append :linked_files, "Passengerfile.json" #"config/database.yml", 'config/master.key'
 
 # Default value for linked_dirs is []
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", "vendor", "storage"
