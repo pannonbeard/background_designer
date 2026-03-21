43e3a77ced313e7419ef1cd7cb27c28f8d362a13
Phil Hendricks <pfhendri1@gmail.com>
2026-03-20 20:55:53 -0500

    fixing url options

--- app/controllers/switcher_controller.rb
+++ app/controllers/switcher_controller.rb
@@ -1,6 +1,6 @@
 class SwitcherController < ApplicationController
   def index
-    ActiveStorage::Current.url_options = Rails.env.development? ? { host: 'localhost', port: 3000 } : { host: '192.168.68.112', port: 80 }
+    ActiveStorage::Current.url_options = Rails.env.development? ? { host: 'localhost', port: 3000 } : { host: 'tableav.ttrpgs.life', port: 443 }
     @scene = Scene.find_by(slug: params[:scene_slug])
     @html_string = render_to_string('viewer/index', layout: false)
     ActionCable.server.broadcast("change_viewer", { slug: params[:scene_slug], html: @html_string })
