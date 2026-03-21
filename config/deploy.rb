48067ae636500c71efddf87e6631a8ea1085fb2d
Phil Hendricks <pfhendri1@gmail.com>
2026-03-20 20:48:26 -0500

    added solid queue

--- config/deploy.rb
+++ config/deploy.rb
@@ -46,3 +46,41 @@ namespace :deploy do
     end
   end
 end
+
+set :solid_queue_systemd_unit_name, "my_app_solid_queue.service"
+
+namespace :solid_queue do
+  desc "Quiet solid_queue (start graceful termination)"
+  task :quiet do
+    on roles(:app) do
+      execute :sudo, "systemctl", "kill", "-s", "SIGTERM", fetch(:solid_queue_systemd_unit_name), raise_on_non_zero_exit: false
+    end
+  end
+
+  desc "Stop solid_queue (force immediate termination)"
+  task :stop do
+    on roles(:app) do
+      execute  :sudo, "systemctl", "kill", "-s", "SIGQUIT", fetch(:solid_queue_systemd_unit_name), raise_on_non_zero_exit: false
+    end
+  end
+
+  desc "Start solid_queue"
+  task :start do
+    on roles(:app) do
+      execute  :sudo, "systemctl", "start", fetch(:solid_queue_systemd_unit_name)
+    end
+  end
+
+  desc "Restart solid_queue"
+  task :restart do
+    on roles(:app) do
+      execute  :sudo, "systemctl", "restart", fetch(:solid_queue_systemd_unit_name)
+    end
+  end
+end
+
+# SolidQueue hooks
+after "deploy:starting", "solid_queue:quiet"
+after "deploy:updated", "solid_queue:stop"
+after "deploy:published", "solid_queue:start"
+after "deploy:failed", "solid_queue:restart"
